a55aa5e40e2b3b22a6f59d782e840262a0b79006
polyzos <polyzosg92@gmail.com>
2019-06-06 19:44:26 +0300

    new files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.21"
+    kotlin("jvm") version "1.3.31"
 }
 
 repositories {
@@ -10,6 +10,18 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+
+    implementation(project(":thesis-commons"))
+
+    // Spark
+    compile(group="org.apache.spark", name="spark-core_2.12", version= "2.4.0")
+    compile(group="org.apache.spark", name="spark-sql_2.12", version= "2.4.0")
+
+    compile("com.natpryce:konfig:1.6.10.0")
+
+//    implementation("jp.nephy:penicillin:4.1.3-eap-2")
+    implementation("io.ktor:ktor-client-cio:1.1.3")
 }
 
 allprojects {
@@ -30,3 +42,4 @@ allprojects {
         useJUnitPlatform()
     }
 }
+
